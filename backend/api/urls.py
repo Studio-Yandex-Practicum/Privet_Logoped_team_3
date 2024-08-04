7d70e9d0230f937e1fff091c5425aed4780f8bdc
Константин Гурашкин <crocko@mail.ru>
2024-08-04 10:39:18 +0300

    first db migration

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,6 +8,11 @@ app_name = 'api'
 router_v1 = routers.DefaultRouter()
 router_v1.register('profile', ProfileViewSet, basename='profile')
 router_v1.register('content', ContentViewSet, basename='content')
+# router_v1.register('locations', LocationViewSet, basename='location')
+# router_v1.register('posts/(?P<post_id>\\d+)/comments',
+#                    CommentViewSet,
+#                    basename='comments')
+# router_v1.register('groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
