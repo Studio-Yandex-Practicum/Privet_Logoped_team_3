8c644c468ac87646e66e53691b356573d1aab336
Victor Ryabov <victorwillhard@gmail.com>
2024-08-07 20:22:16 +0300

    feat(keyboards): add inline keyboard for contact confirmation

--- telegram_bot/keyboards.py
+++ telegram_bot/keyboards.py
@@ -1,47 +1,47 @@
-from aiogram.types import (
-    ReplyKeyboardMarkup,  # Клавиатура под полем ввода
-    KeyboardButton,  # Кнопки для Reply клавиатуры
-)
+from aiogram.types import KeyboardButton  # Кнопки для Reply клавиатуры
+from aiogram.types import ReplyKeyboardMarkup  # Клавиатура под полем ввода
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 main_kb = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text="Полезное видео"),
-            KeyboardButton(text="Отследить результаты")
+            KeyboardButton(text='Полезное видео'),
+            KeyboardButton(text='Отследить результаты'),
         ],
+        [KeyboardButton(text='Оплата'), KeyboardButton(text='Уведомления')],
         [
-            KeyboardButton(text="Оплата"),
-            KeyboardButton(text="Уведомления")
+            KeyboardButton(text='Подарок'),
+            KeyboardButton(text='Помощь с приложением'),
         ],
-        [
-            KeyboardButton(text="Подарок"),
-            KeyboardButton(text="Помощь с приложением")
-        ],
-        [
-            KeyboardButton(text="Связаться с логопедом")
-        ]
+        [KeyboardButton(text='Связаться с логопедом')],
     ],
     # Подстраивает размер кнопок под телефон
     resize_keyboard=True,
     # Скрывает клавиатуру после нажатия кнопки
     one_time_keyboard=True,
     # выводит сообщение в поле ввода во время работы с меню
-    input_field_placeholder="Выберите действие из меню",
-
+    input_field_placeholder='Выберите действие из меню',
 )
 
 
 role_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [
-            KeyboardButton(text="Родитель"),
-            KeyboardButton(text="Логопед")
-        ]
+        [KeyboardButton(text='Родитель'), KeyboardButton(text='Логопед')]
     ],
     # Подстраивает размер кнопок под телефон
     resize_keyboard=True,
     # Скрывает клавиатуру после нажатия кнопки
     one_time_keyboard=True,
     # выводит сообщение в поле ввода во время работы с меню
-    input_field_placeholder="Выберите роль",
+    input_field_placeholder='Выберите роль',
+)
+
+
+confirmation_contact_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Да', url='https://t.me/BotFather'),
+            InlineKeyboardButton(text='Нет', callback_data='main_menu'),
+        ]
+    ]
 )
