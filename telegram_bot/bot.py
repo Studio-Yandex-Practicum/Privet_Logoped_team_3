e5d5c2185af877ff138ff0939b92e4cea6c9c9d8
Victor Ryabov <victorwillhard@gmail.com>
2024-08-07 20:21:49 +0300

    refactor(bot.py): remove unused imports and update code formatting

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,46 +1,32 @@
-import keyboards
-
-from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
 from aiogram.filters import Command
-
+from aiogram.types import Message
+from handlers import router
 from config import bot_env
-
-load_dotenv('.env')
-
+import keyboards
 
 bot = Bot(bot_env.bot_token)
 dp = Dispatcher()
+dp.include_router(router)
 
 
-@dp.message(Command("start"))
+@dp.message(Command('start'))
 async def start(message: Message):
     await message.answer(
         (
             'Добро пожаловать в бота "Привет, Логопед!".\n'
             'Давайте познакомимся, выберите свою роль:'
         ),
-        reply_markup=keyboards.role_kb
+        reply_markup=keyboards.role_kb,
     )
 
 
-@dp.message(Command("menu"))
+@dp.message(Command('menu'))
 async def menu(message: Message):
     await message.answer(
-        (
-            'Вот с чем я могу Вам помочь:'
-        ),
-        reply_markup=keyboards.main_kb
+        'Вот с чем я могу Вам помочь:',
+        reply_markup=keyboards.main_kb,
     )
-
-
-@dp.message()
-async def role(message: Message):
-    msg = message.text.lower()
-
-    if msg in ["родитель", "логопед"]:
-        await menu(message)
 
 
 async def main():
