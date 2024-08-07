e0e88efaf0a278b7bcbbe5eac7f561eea33763d7
Константин Гурашкин <crocko@mail.ru>
2024-08-07 15:36:36 +0300

    feat: create/modify user on vk_bot

--- vk_bot/api/api.py
+++ vk_bot/api/api.py
@@ -22,47 +22,47 @@ class Roles:
         return response
 
     @staticmethod
-    async def _post_id_with_role(user_id, role):
+    async def _post_id_with_role(user_id, role, username):
         response = await async_http_post(
             bot_env.url_api + 'profile/uid/',
-            data=Roles.fill_user_data(user_id, role)
+            data=Roles.fill_user_data(user_id, role, username)
         )
         print(response)
         log.info('Response: %s', response)
         return response
 
     @staticmethod
-    async def send_role_for_id(user_id, role):
+    async def send_role_for_id(user_id, role, username):
         user_id = str(user_id)
         response = await Roles._get_id(user_id)
         print(response)
         if response['status'] == HTTPStatus.NOT_FOUND:
             log.info('Not found user, create new')
-            await Roles._post_id_with_role(user_id, role)
+            await Roles._post_id_with_role(user_id, role, username)
 
         elif response['status'] == HTTPStatus.OK:
             current_user = UserProfile.parse_raw(response['text'])
             if not Roles.check_user_role(current_user.role, role):
                 log.info('User has not required role, update')
-                response = await Roles._modify_user(user_id, role)
+                response = await Roles._modify_user(user_id, role, username)
 
         log.info('Response: %s', response)
         return True
 
     @staticmethod
-    async def _modify_user(user_id, role):
+    async def _modify_user(user_id, role, username):
         response = await async_http_put(
             bot_env.url_api + 'profile/uid/' + user_id + '/',
-            data=Roles.fill_user_data(user_id, role)
+            data=Roles.fill_user_data(user_id, role, username)
         )
         print(response)
         return response
 
     @staticmethod
-    def fill_user_data(user_id, role):
+    def fill_user_data(user_id, role, username):
         return {
             'user_id': user_id,
-            'username': user_id,
+            'username': username,
             'platform': 'vk',
             'role': Roles.role(role)
         }
