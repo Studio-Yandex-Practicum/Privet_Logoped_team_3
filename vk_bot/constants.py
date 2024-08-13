351678d6b437bad219dddd7669762bf88bb6e5dc
Vlad_Work <inf.vlad.s@bk.ru>
2024-08-13 09:52:18 +0300

    Убрал telegram_bot и vkbot

--- vk_bot/constants.py
+++
@@ -1,8 +0,0 @@
-DB_PATH = 'sqlite:///sqlite.db'
-
-USER_TOKEN_MAX_LEN = 15
-
-COMMAND_PREFIXES = ['', '/']
-MAIN_MENU_WORD = 'меню'
-GREETING_MESSAGE = ('Добро пожаловать в бота “Привет, Логопед!”. '
-                    'Пожалуйста выберите действия:”')
