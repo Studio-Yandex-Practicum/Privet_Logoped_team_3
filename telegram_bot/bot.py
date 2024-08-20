a3ab29bd8726cc40687725271dfd0dd6fdeca94a
Victor Ryabov <victorwillhard@gmail.com>
2024-08-20 18:31:06 +0300

    feat: make job for notify users

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,3 +1,7 @@
+import aiohttp
+import asyncio
+from datetime import datetime, UTC
+
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -13,6 +17,28 @@ bot = Bot(bot_env.bot_token)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 dp.include_router(router)
+
+
+async def notify_users():
+    utc_time = datetime.now(UTC)
+    utc_time = f'{utc_time.hour}:{utc_time.minute}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f'{bot_env.host}/api/v1/notification/tg/?time={utc_time}'
+        ) as response:
+            if response.status == 200:
+                date = await response.json()
+                if not date:
+                    return
+                for user in date:
+                    user_uid = user.get('uid')
+                    await bot.send_message(user_uid, lexicon.messages.notification)
+
+
+async def notify_users_job():
+    while True:
+        await notify_users()
+        await asyncio.sleep(60)
 
 
 @dp.message(Command('start'))
@@ -32,5 +58,9 @@ async def menu(message: Message):
 
 
 async def main():
+    notification_job = asyncio.create_task(notify_users_job())
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        notification_job.cancel()
